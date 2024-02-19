9524475b250eb3eb6f505e375b2a21eeaf2d4d91
Mark Fraser <markfraser2015@yahoo.com>
2024-02-18 22:03:05 -0600

    [WIP] Initializing I2C comms with the ADT7420
    Working on properly initializing and resolving which fields
    correspond to what functionality in `surf`'s `I2cMaster` module.
    
    The temp sensor's temperature signals have been mapped directly to the
    output LEDs to start, then will be refactored to use red and green for
    better user feedback.
    
    This commit does NOT currently build.

--- firmware/targets/TemperatureSensorReader/hdl/TemperatureSensorReader.vhd
+++ firmware/targets/TemperatureSensorReader/hdl/TemperatureSensorReader.vhd
@@ -1,22 +1,89 @@
 library ieee;
 use ieee.std_logic_1164.all;
 
+library surf;
+use surf.I2cPkg.all;
+use surf.I2cMaster;
+
 library mf;
-use mf.segment_display.all;
+use mf.SegmentDisplay;
 
 entity TemperatureSensorReader is
-    Port ( CLK100MHZ : in STD_LOGIC;
-           CPU_RESETN : in STD_LOGIC;
-           AN : out STD_LOGIC_VECTOR (7 downto 0);
-           CA : out STD_LOGIC_VECTOR (7 downto 0)
-         );
+    Port (
+        CLK100MHZ : in STD_LOGIC;
+        CPU_RESETN : in STD_LOGIC;
+
+        -- Segment display anodes and cathodes
+        AN : out STD_LOGIC_VECTOR (7 downto 0);
+        CA : out STD_LOGIC_VECTOR (7 downto 0);
+
+        -- Temperature sensor I2C pins
+        TMP_SCL : inout STD_LOGIC;
+        TMP_SDA : inout STD_LOGIC;
+
+        -- Temperature sensor error signals
+        TMP_INT : in STD_LOGIC;
+        TMP_CT  : in STD_LOGIC;
+
+        -- LEDs to display
+        LED16_R : out STD_LOGIC;
+        LED17_R : out STD_LOGIC
+    );
 end TemperatureSensorReader;
 
 architecture Behavioral of TemperatureSensorReader is
+    --! I2C slave address for the ADT7420 temperature sensor.
+    --!
+    constant ADT7420_I2C_SLAVE_ADDR : std_logic_vector (9 downto 0) := X"4B";
 
+    --! I2C master (FPGA chip) input fields.
+    --!
+    signal i2cMasterIn : I2cMasterInType := (
+        enable => '1',
+        -- prescale => ?
+        -- filter => ?
+        -- txnReq => ?
+        stop => '0',
+        op => '0', -- read
+        busReq => '0', -- read/write
+        addr => ADT7420_I2C_SLAVE_ADDR
+        -- tenbit => '0',
+        -- wrValid => ?
+        -- wrData => ?
+        -- rdAck => ?
+    );
+
+    --! Output fields from the I2C slave (ADT7420)
+    signal i2cMasterOut : I2cMasterOutType := (
+        busAck => '0',
+        txnError => '0',
+        wrAck => '0',
+        rdValid => '0',
+        rdData => "00000000"
+    );
+
+    --! Two-byte register store for the last read temperature
+    --!
     signal temperature : STD_LOGIC_VECTOR (15 downto 0);
 
 begin
+
+    -- Temperature signals are directly written to the LEDs for visibility of
+    -- temperature sensor errors.
+    LED16_R <= TMP_INT;
+    LED17_R <= TMP_CT;
+
+adt7420:
+    entity I2cMaster
+    port map (
+        clk => CLK100MHZ,
+        i2cMasterIn => i2cMasterIn,
+        i2cMasterOut => i2cMasterOut
+
+        --i2co => (
+
+        --)
+    );
 
 display:
     entity SegmentDisplay
@@ -26,5 +93,12 @@ display:
         anodes => AN,
         cathodes => CA
     );
+
+--MAIN:
+--    process(CLK100MHZ, CPU_RESETN, cur_val)
+--    begin
+--        if rising_edge(CLK100MHZ) then
+--        end if;
+--    end process;
 
 end Behavioral;
--- firmware/targets/TemperatureSensorReader/hdl/TemperatureSensorReader.xdc
+++ firmware/targets/TemperatureSensorReader/hdl/TemperatureSensorReader.xdc
@@ -47,10 +47,10 @@ create_clock -add -name sys_clk_pin -period 10.00 -waveform {0 5} [get_ports {CL
 ## RGB LEDs
 #set_property -dict { PACKAGE_PIN R12   IOSTANDARD LVCMOS33 } [get_ports { LED16_B }]; #IO_L5P_T0_D06_14 Sch=led16_b
 #set_property -dict { PACKAGE_PIN M16   IOSTANDARD LVCMOS33 } [get_ports { LED16_G }]; #IO_L10P_T1_D14_14 Sch=led16_g
-#set_property -dict { PACKAGE_PIN N15   IOSTANDARD LVCMOS33 } [get_ports { LED16_R }]; #IO_L11P_T1_SRCC_14 Sch=led16_r
+set_property -dict { PACKAGE_PIN N15   IOSTANDARD LVCMOS33 } [get_ports { LED16_R }]; #IO_L11P_T1_SRCC_14 Sch=led16_r
 #set_property -dict { PACKAGE_PIN G14   IOSTANDARD LVCMOS33 } [get_ports { LED17_B }]; #IO_L15N_T2_DQS_ADV_B_15 Sch=led17_b
 #set_property -dict { PACKAGE_PIN R11   IOSTANDARD LVCMOS33 } [get_ports { LED17_G }]; #IO_0_14 Sch=led17_g
-#set_property -dict { PACKAGE_PIN N16   IOSTANDARD LVCMOS33 } [get_ports { LED17_R }]; #IO_L11N_T1_SRCC_14 Sch=led17_r
+set_property -dict { PACKAGE_PIN N16   IOSTANDARD LVCMOS33 } [get_ports { LED17_R }]; #IO_L11N_T1_SRCC_14 Sch=led17_r
 
 ##7 segment display
 #set_property -dict { PACKAGE_PIN T10   IOSTANDARD LVCMOS33 } [get_ports { CA }]; #IO_L24N_T3_A00_D16_14 Sch=ca
